e18a640c08fe3a09fdec2ce513d71fafd627e9ec
Joel Fickson <Joelngozo@gmail.com>
2025-07-10 07:25:42 +0200

    feat: enhance middleware configuration and update Cloudflare R2 plugin settings

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,12 +1,54 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            process.env.CF_PUBLIC_ACCESS_URL?.replace(/\/$/, '') || '',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            process.env.CF_PUBLIC_ACCESS_URL?.replace(/\/$/, '') || '',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // modify form body
+      jsonLimit: '256mb', // modify JSON body
+      textLimit: '256mb', // modify text body
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
